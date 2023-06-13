10ba4a374e490de143bb5e27ca63bf947a528d0a
inyeong-kang <tomatorkd@naver.com>
2023-06-14 02:27:30 +0900

    chore: change package setting, site configuration

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,22 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd1e89e9e42eb4ebf9486ae0374039efc',
-
+  rootNotionPageId: 'dc81382d26324a8c8f8844ec4bf78836',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '2skydev blog',
-  domain: 'blog.2skydev.com',
-  author: '2skydev',
+  name: 'jero blog',
+  domain: 'blog.jero.com',
+  author: 'jero',
 
   // open graph metadata (optional)
-  description: '2skydev blog - developer blog',
+  description: 'jero blog - developer blog',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  github: '2skydev',
+  github: 'jero-kang',
   // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -54,8 +53,8 @@ export default siteConfig({
   navigationLinks: [
     {
       title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
-    },
+      pageId: '204a5f2038f749b1b15fe9a6761fbca6'
+    }
   ],
 
   // -------- custom configs (2skydev) -------------
